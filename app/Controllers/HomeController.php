46b0edba3cc9a58b9eab562553cf5dcdab4dd74d
Yanik Kumar <yanikkumar@cdac.in>
2025-07-14 17:04:58 +0530

    Some UI and animations updates

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController
     {
         $release = getLatestRelease();
         $appName = config('app.name', 'SproutPHP');
-        return "<p>A minimilistic php-framework designed for go-to developer without the need for javascript or heavy modules. 🌳</p>
-        <small>{$appName} latest release: <strong>$release</strong></small>";
+        return "<div style='text-align: center'><p>A minimilistic php-framework designed for go-to developer without the need for javascript or heavy modules. 🌳</p>
+        <small>{$appName} latest release: <strong>$release</strong></small></div>";
     }
 }
